82427eba1c851f2d4edf70088675dbb4d62ec365
Randall Booth <dirid51@gmail.com>
2014-12-31 14:00:08 -0600

    first commit

---
+++ src/edu/ksu/sandbox/RemoveSpecials.java
@@ -0,0 +1,23 @@
+package edu.ksu.sandbox;
+
+public class RemoveSpecials {
+
+	public static void main(String[] args) {
+		RemoveSpecials rs = new RemoveSpecials();
+		System.out.println(rs.removeSpecials("What Do You Envision in Life?"));
+
+	}
+	
+	public String removeSpecials(String input) {
+		StringBuilder result = new StringBuilder();
+		
+		for (Character c : input.toCharArray()) {
+			if (Character.isDigit(c) || Character.isLetter(c) || Character.isSpaceChar(c)) {
+				result.append(c);
+			}
+		}
+		
+		return result.toString();
+	}
+
+}
