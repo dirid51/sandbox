7389533a1b9156f51958a45507e9ada9f777fda0
Randall Booth <dirid51@gmail.com>
2015-12-07 17:10:44 -0600

    Updated sales tax calculator

--- src/org/dirid51/sandbox/SalesTaxCalculator.java
+++ src/org/dirid51/sandbox/SalesTaxCalculator.java
@@ -1,25 +1,28 @@
 package org.dirid51.sandbox;
 
 import java.math.BigDecimal;
-
-import org.apache.commons.lang3.StringUtils;
+import java.math.RoundingMode;
 
 public class SalesTaxCalculator {
 
 	public static void main(String[] args) {
-		if (args.length == 3 && StringUtils.isNumeric(args[1]) && StringUtils.isNumeric(args[2])) {
-			BigDecimal totalSales = new BigDecimal(args[1]);
-			BigDecimal salesTaxPercentage = new BigDecimal(1 + (Float.valueOf(args[2]) / 100.0));
-			BigDecimal earned = totalSales.divide(salesTaxPercentage);
-			BigDecimal salesTaxDollars = totalSales.subtract(earned);
-			System.out.println("\tReceived:\t\t" + totalSales.toPlainString());
-			System.out.println("\tKeeping:\t\t" + earned.toPlainString());
-			System.out.println("\tSales Tax:\t\t" + salesTaxDollars.toPlainString());
+		if (args.length == 2) {
+			BigDecimal totalSales = new BigDecimal(args[0]).setScale(10, RoundingMode.CEILING);
+			System.out.println("totalSales: " + totalSales.toPlainString());
+			BigDecimal taxPercentage = new BigDecimal(args[1]).movePointLeft(2);
+			System.out.println("taxPercentage: " + taxPercentage.toPlainString());
+			BigDecimal onePlusTaxPercent = BigDecimal.ONE.add(taxPercentage);
+			System.out.println("onePlusTaxPercent: " + onePlusTaxPercent.toPlainString());
+			BigDecimal earned = totalSales.divide(onePlusTaxPercent, RoundingMode.CEILING);
+			System.out.println("earned: " + earned.toPlainString());
+			BigDecimal salesTax = totalSales.subtract(earned).setScale(2, RoundingMode.CEILING);
+			System.out.println("salesTax: " + salesTax.toPlainString());
 		} else {
 			System.out.println("Invalid usage.");
 			System.out.println();
 			System.out.println("Correct usage:");
 			System.out.println("java SalesTaxCalculator <total sales> <sales tax percentage>");
+			System.out.println("Example:\t\tjava SalesTaxCalculator 520.75 9.1");
 			System.out.println();
 		}
 	}
